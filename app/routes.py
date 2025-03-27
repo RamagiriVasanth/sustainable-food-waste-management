bf1ffda488204d6747683b4c69bed8103f857eb2
RamagiriVasanth <ramagirivasanth111@gmail.com>
2025-03-27 21:10:21 +0530

    Initial commit for Food Waste Management System

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# In-memory storage for food items
 food_items = []
 
 @app.route('/')
@@ -11,21 +10,18 @@ def index():
 
 @app.route('/add_food', methods=['POST'])
 def add_food():
-    # Get data from the form
-    food_name = request.form.get('food_name')
-    expiration_date = request.form.get('expiration_date')
+    food_name = request.form['food_name']
+    expiration_date = request.form['expiration_date']
     
-    # Add the new food item to the list
+    # Adding food item to the list
     food_items.append({
         'name': food_name,
         'expiration_date': expiration_date
     })
-    
     return redirect(url_for('index'))
 
 @app.route('/clear', methods=['POST'])
 def clear_items():
-    # Clear all food items
     food_items.clear()
     return redirect(url_for('index'))
 
